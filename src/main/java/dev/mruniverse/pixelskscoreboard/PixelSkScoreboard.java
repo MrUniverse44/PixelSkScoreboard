6da37f3aa82ca623407c8a00a805a2172ca2cf0d
MrUniverse44 <justinalxhd@gmail.com>
2021-02-12 23:22:16 -0500

    Initial commit

---
+++ src/main/java/dev/mruniverse/pixelskscoreboard/PixelSkScoreboard.java
@@ -0,0 +1,10 @@
+package dev.mruniverse.pixelskscoreboard;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PixelSkScoreboard extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+}
