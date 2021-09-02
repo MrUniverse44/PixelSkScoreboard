5452d43389914acddbb5506d995650687c607a3a
MrUniverse44 <oficial.mruniverse@gmail.com>
2021-09-02 15:09:19 -0500

    Added Support to 1.17

--- src/main/java/dev/mruniverse/pixelskscoreboard/netherboard/util/NMS.java
+++ src/main/java/dev/mruniverse/pixelskscoreboard/netherboard/util/NMS.java
@@ -2,7 +2,8 @@ package dev.mruniverse.pixelskscoreboard.netherboard.util;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
@@ -10,11 +11,13 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
-@SuppressWarnings("unused")
+
 public class NMS {
 
-    private static final String packageName;
-    private static final Version version;
+    private static final Logger LOGGER = LoggerFactory.getLogger(NMS.class);
+
+    private static final String OLD_PACKAGE_NAME;
+    private static final Version VERSION;
 
     public static final Field PLAYER_SCORES;
 
@@ -35,11 +38,9 @@ public class NMS {
 
     static {
         String name = Bukkit.getServer().getClass().getPackage().getName();
-
         String ver = name.substring(name.lastIndexOf('.') + 1);
-        version = new Version(ver);
-
-        packageName = "net.minecraft.server." + ver;
+        VERSION = new Version(ver);
+        OLD_PACKAGE_NAME = "net.minecraft.server." + ver;
 
         Field playerScores = null;
 
@@ -59,55 +60,67 @@ public class NMS {
         Method sendPacket = null;
 
         try {
-            Class<?> packetScoreClass = getClass("PacketPlayOutScoreboardScore");
-            Class<?> packetDisplayClass = getClass("PacketPlayOutScoreboardDisplayObjective");
-            Class<?> packetObjClass = getClass("PacketPlayOutScoreboardObjective");
+            Class<?> packetScoreClass = getClass("net.minecraft.network.protocol.game", "PacketPlayOutScoreboardScore");
+            Class<?> packetDisplayClass = getClass("net.minecraft.network.protocol.game", "PacketPlayOutScoreboardDisplayObjective");
+            Class<?> packetObjClass = getClass("net.minecraft.network.protocol.game", "PacketPlayOutScoreboardObjective");
 
-            Class<?> scoreClass = getClass("ScoreboardScore");
-            Class<?> scoreActionClass = getClass("ScoreboardServer$Action");
+            Class<?> scoreClass = getClass("net.minecraft.world.scores", "ScoreboardScore");
 
-            Class<?> sbClass = getClass("Scoreboard");
-            Class<?> objClass = getClass("ScoreboardObjective");
+            Class<?> sbClass = getClass("net.minecraft.world.scores", "Scoreboard");
+            Class<?> objClass = getClass("net.minecraft.world.scores", "ScoreboardObjective");
 
-            Class<?> playerClass = getClass("EntityPlayer");
-            Class<?> playerConnectionClass = getClass("PlayerConnection");
-            Class<?> packetClass = getClass("Packet");
-            if(sbClass != null && scoreClass != null && packetObjClass != null & packetScoreClass != null) {
+            Class<?> playerClass = getClass("net.minecraft.server.level", "EntityPlayer");
+            Class<?> playerConnectionClass = getClass("net.minecraft.server.network", "PlayerConnection");
+            Class<?> packetClass = getClass("net.minecraft.network.protocol", "Packet");
+
+
+            sbScore = scoreClass.getConstructor(sbClass, objClass, String.class);
+            sbScoreSet = scoreClass.getMethod("setScore", int.class);
+
+            packetObj = packetObjClass.getConstructor(objClass, int.class);
+
+            packetDisplay = packetDisplayClass.getConstructor(int.class, objClass);
+
+            sendPacket = playerConnectionClass.getMethod("sendPacket", packetClass);
+
+            if (VERSION.isBelow1_17()) {
                 playerScores = sbClass.getDeclaredField("playerScores");
                 playerScores.setAccessible(true);
 
-                sbScore = scoreClass.getConstructor(sbClass, objClass, String.class);
-                sbScoreSet = scoreClass.getMethod("setScore", int.class);
-
-                packetObj = packetObjClass.getConstructor(objClass, int.class);
-
-                switch (version.getMajor()) {
-                    case "1.7":
-                        packetScore = packetScoreClass.getConstructor(scoreClass, int.class);
-                        break;
-                    case "1.8":
-                    case "1.9":
-                    case "1.10":
-                    case "1.11":
-                    case "1.12":
-                        packetScore = packetScoreClass.getConstructor(scoreClass);
-                        packetScoreRemove = packetScoreClass.getConstructor(String.class, objClass);
-                        break;
-                    default:
-                        packetScore = packetScoreClass.getConstructor(scoreActionClass,
-                                String.class, String.class, int.class);
-                        if(scoreActionClass != null) {
-                            enumScoreActionChange = scoreActionClass.getEnumConstants()[0];
-                            enumScoreActionRemove = scoreActionClass.getEnumConstants()[1];
-                        }
-                        break;
-                }
-                if(packetDisplayClass != null) packetDisplay = packetDisplayClass.getConstructor(int.class, objClass);
-
-                if(playerClass != null) playerConnection = playerClass.getField("playerConnection");
-                if(playerConnectionClass != null) sendPacket = playerConnectionClass.getMethod("sendPacket", packetClass);
+                playerConnection = playerClass.getField("playerConnection");
             }
-        } catch(Throwable ignored) {}
+            else {
+                playerScores = sbClass.getDeclaredField("j");
+                playerScores.setAccessible(true);
+
+                playerConnection = playerClass.getField("b");
+            }
+
+            switch (VERSION.getMajor()) {
+                case "1.7":
+                    packetScore = packetScoreClass.getConstructor(scoreClass, int.class);
+                    break;
+                case "1.8":
+                case "1.9":
+                case "1.10":
+                case "1.11":
+                case "1.12":
+                    packetScore = packetScoreClass.getConstructor(scoreClass);
+                    packetScoreRemove = packetScoreClass.getConstructor(String.class, objClass);
+                    break;
+                default:
+                    Class<?> scoreActionClass = getClass("net.minecraft.server", "ScoreboardServer$Action");
+
+                    packetScore = packetScoreClass.getConstructor(scoreActionClass,
+                            String.class, String.class, int.class);
+
+                    enumScoreActionChange = scoreActionClass.getEnumConstants()[0];
+                    enumScoreActionRemove = scoreActionClass.getEnumConstants()[1];
+                    break;
+            }
+        } catch(Exception e) {
+            LOGGER.error("Error while loading NMS methods. (Unsupported Minecraft version?)", e);
+        }
 
         PLAYER_SCORES = playerScores;
 
@@ -128,33 +141,31 @@ public class NMS {
     }
 
     public static Version getVersion() {
-        return version;
+        return VERSION;
     }
 
-    public static Class<?> getClass(String name) {
-        try {
-            return Class.forName(packageName + "." + name);
-        } catch(ClassNotFoundException e) {
-            return null;
-        }
+    public static Class<?> getClass(String realPackageName, String name) throws ClassNotFoundException {
+        String packageName = VERSION.isBelow1_17() ? OLD_PACKAGE_NAME : realPackageName;
+
+        return Class.forName(packageName + "." + name);
     }
 
-    private static final Map<Class<?>, Method> handles = new HashMap<>();
+    private static final Map<Class<?>, Method> HANDLES = new HashMap<>();
     public static Object getHandle(Object obj) throws NoSuchMethodException,
             InvocationTargetException, IllegalAccessException {
 
         Class<?> clazz = obj.getClass();
 
-        if(!handles.containsKey(clazz)) {
+        if(!HANDLES.containsKey(clazz)) {
             Method method = clazz.getDeclaredMethod("getHandle");
 
             if(!method.isAccessible())
                 method.setAccessible(true);
 
-            handles.put(clazz, method);
+            HANDLES.put(clazz, method);
         }
 
-        return handles.get(clazz).invoke(obj);
+        return HANDLES.get(clazz).invoke(obj);
     }
 
     public static void sendPacket(Object packet, Player... players) {
@@ -162,7 +173,9 @@ public class NMS {
             try {
                 Object playerConnection = PLAYER_CONNECTION.get(getHandle(p));
                 SEND_PACKET.invoke(playerConnection, packet);
-            } catch(Throwable ignored) {}
+            } catch(IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+                LOGGER.error("Error while sending packet to player. (Unsupported Minecraft version?)", e);
+            }
         }
     }
 
@@ -176,16 +189,30 @@ public class NMS {
         Version(String name) {
             this.name = name;
 
-            String[] splitted = name.split("_");
+            String[] splitName = name.split("_");
 
-            this.major = splitted[0].substring(1) + "." + splitted[1];
-            this.minor = splitted[2].substring(1);
+            this.major = splitName[0].substring(1) + "." + splitName[1];
+            this.minor = splitName[2].substring(1);
         }
 
         public String getName() { return name; }
 
         public String getMajor() { return major; }
+        @SuppressWarnings("unused")
         public String getMinor() { return minor; }
+
+        public boolean isBelow1_17() {
+            return  major.equals("1.7")  ||
+                    major.equals("1.8")  ||
+                    major.equals("1.9")  ||
+                    major.equals("1.10") ||
+                    major.equals("1.11") ||
+                    major.equals("1.12") ||
+                    major.equals("1.13") ||
+                    major.equals("1.14") ||
+                    major.equals("1.15") ||
+                    major.equals("1.16");
+        }
 
     }
 
